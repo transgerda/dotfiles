0a2479ab0081537b03cb6a1543ca5e227c9e301f
transgerda <info@martijnvanboven.nl>
2026-05-24 23:45:44 +0200

    ja

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,12 +2,15 @@ if status is-interactive
     zoxide init fish | source
     atuin init fish --disable-up-arrow | source
 
-    alias 'a'='lsd -lah'
-    alias 'lg'=lazygit
+    alias a='lsd -lah'
+    alias lg=lazygit
+    alias ptest='composer dump-autoload && php artisan test'
 
     set -x LD_LIBRARY_PATH /run/current-system/sw/lib $LD_LIBRARY_PATH
+    set -x YDOTOOL_SOCKET /run/ydotoold/socket
     set -x EDITOR nvim
     set -g fish_greeting ""
+    set -Ux PATH $HOME/.local/share/nvim/mason/bin $PATH
 
     fish_vi_key_bindings
 end
---
+++ .config/scripts/rofi/auto-clicker.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+PROC_NAME="autoclicker-loop"
+
+# If running → kill all and exit
+if pgrep -f "$PROC_NAME" > /dev/null; then
+    pkill -f "$PROC_NAME"
+    exit 0
+fi
+
+choice=$(printf "10 CPS Left\n1 CPS Left\n0.1 CPS Left\n10 CPS Right\n1 CPS Right\n0.1 CPS Right" \
+  | rofi -dmenu -p "Auto Clicker" -theme ~/.config/rofi/global.rasi)
+
+get_delay() {
+    awk "BEGIN { print 1/$1 }"
+}
+
+start_clicking() {
+    button="$1"
+    cps="$2"
+    delay=$(get_delay "$cps")
+
+    # Rename this process so we can target it
+    exec -a "$PROC_NAME" bash -c '
+        button="$1"
+        delay="$2"
+
+        while true; do
+            if [[ "$button" == "left" ]]; then
+                ydotool click 0xC0
+            else
+                ydotool click 0xC1
+            fi
+            sleep "$delay"
+        done
+    ' _ "$button" "$delay"
+}
+
+case "$choice" in
+  "10 CPS Left") start_clicking left 10 & ;;
+  "1 CPS Left") start_clicking left 1 & ;;
+  "0.1 CPS Left") start_clicking left 0.1 & ;;
+  "10 CPS Right") start_clicking right 10 & ;;
+  "1 CPS Right") start_clicking right 1 & ;;
+  "0.1 CPS Right") start_clicking right 0.1 & ;;
+esac
